d79e3384f521e4dd58f76f4bcf1465a5ebc43dbe
Shahid N. Shah <shahid@shah.org>
2023-05-16 12:32:51 -0400

    feat: add destroy-first and schema in CLI

--- packages/deno/entity-resolution/er-dv-schema.ts
+++ packages/deno/entity-resolution/er-dv-schema.ts
@@ -1,20 +1,20 @@
 import { Command } from "https://deno.land/x/cliffy@v0.25.7/command/mod.ts";
-import * as SQLa from "https://raw.githubusercontent.com/netspective-labs/sql-aide/v0.0.5/render/mod.ts";
-import * as dvp from "https://raw.githubusercontent.com/netspective-labs/sql-aide/v0.0.5/pattern/data-vault.ts";
-import * as dia from "https://raw.githubusercontent.com/netspective-labs/sql-aide/v0.0.5/render/diagram/plantuml-ie-notation.ts";
+import * as ws from "https://raw.githubusercontent.com/netspective-labs/sql-aide/v0.0.7/lib/universal/whitespace.ts";
+import * as SQLa from "https://raw.githubusercontent.com/netspective-labs/sql-aide/v0.0.7/render/mod.ts";
+import * as dvp from "https://raw.githubusercontent.com/netspective-labs/sql-aide/v0.0.7/pattern/data-vault.ts";
+import * as dia from "https://raw.githubusercontent.com/netspective-labs/sql-aide/v0.0.7/render/diagram/plantuml-ie-notation.ts";
 
 const ctx = SQLa.typicalSqlEmitContext();
 type EmitContext = typeof ctx;
 
-const stso = SQLa.typicalSqlTextSupplierOptions<EmitContext>();
-const dvg = dvp.dataVaultGovn<EmitContext>(stso);
-const { text, textNullable, integer, integerNullable, date } = dvg.domains;
-const { ulidPrimaryKey: primaryKey } = dvg.keys;
+const dvts = dvp.dataVaultTemplateState<EmitContext>();
+const { text, textNullable, integer, integerNullable, date } = dvts.domains;
+const { ulidPrimaryKey: primaryKey } = dvts.keys;
 
-const erEntityHub = dvg.hubTable("er_entity", {
+const erEntityHub = dvts.hubTable("er_entity", {
   hub_er_entity_id: primaryKey(),
   ssn_business_key: text(),
-  ...dvg.housekeeping.columns,
+  ...dvts.housekeeping.columns,
 });
 
 const erAlgorithmLookupTable = SQLa.tableDefinition("er_algorithm", {
@@ -24,10 +24,10 @@ const erAlgorithmLookupTable = SQLa.tableDefinition("er_algorithm", {
   algorithm_sp: text(),
 });
 
-const erJobHub = dvg.hubTable("er_job", {
+const erJobHub = dvts.hubTable("er_job", {
   hub_er_job_id: primaryKey(),
   job_business_job_name: text(),
-  ...dvg.housekeeping.columns,
+  ...dvts.housekeeping.columns,
 });
 
 const erEntityHubSat = erEntityHub.satelliteTable("er_entity_attribute", {
@@ -36,7 +36,7 @@ const erEntityHubSat = erEntityHub.satelliteTable("er_entity_attribute", {
   name: text(),
   address: text(),
   phone: text(),
-  ...dvg.housekeeping.columns
+  ...dvts.housekeeping.columns,
 });
 
 const erJobHubSat = erJobHub.satelliteTable("er_job_state", {
@@ -45,15 +45,15 @@ const erJobHubSat = erJobHub.satelliteTable("er_job_state", {
   algorithm_id: integer(),
   run_date_time: date(),
   status: text(),
-  ...dvg.housekeeping.columns,
+  ...dvts.housekeeping.columns,
 });
 
-const erEntityMatchLink = dvg.linkTable("er_entity_match", {
+const erEntityMatchLink = dvts.linkTable("er_entity_match", {
   link_er_entity_match_id: primaryKey(),
   hub_entity_id: erEntityHubSat.references
     .sat_er_entity_er_entity_attribute_id(),
   algorithm_ref: erAlgorithmLookupTable.references.algorithm_id(),
-  ...dvg.housekeeping.columns,
+  ...dvts.housekeeping.columns,
 });
 
 const erEntityMatchLevenshteinLinkSat = erEntityMatchLink.satelliteTable(
@@ -66,7 +66,7 @@ const erEntityMatchLevenshteinLinkSat = erEntityMatchLink.satelliteTable(
     similarity_score: integer(),
     normalized_distance: integer(),
     notes: text(),
-    ...dvg.housekeeping.columns,
+    ...dvts.housekeeping.columns,
   },
 );
 
@@ -79,46 +79,64 @@ const erEntityMatchSoundexLinkSat = erEntityMatchLink.satelliteTable(
     code: text(),
     similarity_score: integer(),
     index: integer(),
-    ...dvg.housekeeping.columns,
+    ...dvts.housekeeping.columns,
   },
 );
 
-function handleSQL(_options?: {
-  dest?: string | undefined;
-}) {
-/**
- * Template which generates schema objects when none exist; other templates
- * should be defined for schema migrations.
- */
-const seedDDL = SQLa.SQL<EmitContext>(stso)`
-${erAlgorithmLookupTable}
+function sqlDDL(options: {
+  destroyFirst?: boolean;
+  schemaName?: string;
+} = {}) {
+  const { destroyFirst, schemaName } = options;
 
-${erEntityHub}
+  // NOTE: every time the template is "executed" it will fill out tables, views
+  //       in dvts.tablesDeclared, etc.
+  // deno-fmt-ignore
+  return SQLa.SQL<EmitContext>(dvts.ddlOptions)`
+    ${ destroyFirst && schemaName
+       ? `drop schema if exists ${schemaName} cascade;`
+       : "-- not destroying first (for development)" }
+    ${ schemaName
+       ? `create schema if not exists ${schemaName};`
+       : "-- no schemaName provided" }
 
-${erJobHub}
+    ${erAlgorithmLookupTable}
 
-${erJobHubSat}
+    ${erEntityHub}
 
-${erEntityMatchLink}
+    ${erJobHub}
 
-${erEntityMatchLevenshteinLinkSat}
+    ${erJobHubSat}
 
-${erEntityMatchSoundexLinkSat}`;
-  console.log(seedDDL.SQL(ctx));
+    ${erEntityMatchLink}
+
+    ${erEntityMatchLevenshteinLinkSat}
+
+    ${erEntityMatchSoundexLinkSat}`;
 }
 
-function handleDiagram(_options?: {
+function handleSQL(options: {
   dest?: string | undefined;
-}) {
+  destroyFirst?: boolean;
+  schemaName?: string;
+} = {}) {
+  console.log(ws.unindentWhitespace(sqlDDL(options).SQL(ctx)));
+}
+
+function handleDiagram(_options: {
+  dest?: string | undefined;
+} = {}) {
+  // "executing" the following will fill dvts.tablesDeclared but we don't
+  // care about the SQL output, just the state management
+  sqlDDL().SQL(ctx);
   const pumlERD = dia.plantUmlIE(ctx, function* () {
-    yield erAlgorithmLookupTable.graphEntityDefn();
-    yield erEntityHub.graphEntityDefn();
-    yield erJobHub.graphEntityDefn();
-    yield erEntityMatchLink.graphEntityDefn();
-    yield erEntityMatchLevenshteinLinkSat.graphEntityDefn();
-    yield erEntityMatchSoundexLinkSat.graphEntityDefn();
+    for (const table of dvts.tablesDeclared) {
+      if (SQLa.isGraphEntityDefinitionSupplier(table)) {
+        yield table.graphEntityDefn();
+      }
+    }
   }, dia.typicalPlantUmlIeOptions());
-  console.log(pumlERD.content)
+  console.log(pumlERD.content);
 }
 
 // deno-fmt-ignore
@@ -129,6 +147,8 @@ await new Command()
   .action(() => handleSQL())
   .command("sql", "Emit SQL")
     .option("-d, --dest <file:string>", "Output destination, STDOUT if not supplied")
+    .option("--destroy-first", "Destroy an previous objects first (useful for development)")
+    .option("--schema-name <schemaName:string>", "If destroying or creating a schema, this is the name of the schema")
     .action((options) => handleSQL(options))
   .command("diagram", "Emit Diagram")
     .option("-d, --dest <file:string>", "Output destination, STDOUT if not supplied")
